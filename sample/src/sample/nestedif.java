82d94acf9590ec2532e497dfb722fa1e33599f69
Soujanya <anipeddisoujanya@gmail.com>
2023-08-29 21:56:08 +0530

    eligibleforvote

---
+++ sample/src/sample/nestedif.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class nestedif {
+
+	public static void main(String[] args) {
+	  int age=39;
+	  if(age>18)
+	  {
+		  System.out.println("eligible for vote");
+	  }
+	  else {
+		  System.out.println("not eligible");
+	  }
+
+	}
+
+}
